4739444f0fbf76eda44d1a086dbdcfe806c24363
Prajwal Urs <prajwal@enhanzed.com>
2021-10-19 17:33:01 +0530

    socket.io 6

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
 }
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 httpServer.listen(port, (error) => {
   if (error) throw error;
