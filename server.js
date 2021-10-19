54983bd7161475af0a70bb2b43a120374a4545e7
Prajwal Urs <prajwal@enhanzed.com>
2021-10-19 15:33:43 +0530

    Update code

--- server.js
+++ server.js
@@ -10,13 +10,12 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
 }
+app.use(express.static(path.join(__dirname, "build")));
 
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.listen(port, (error) => {
   if (error) throw error;
   console.log("Server running on port " + port);
