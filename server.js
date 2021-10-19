a3d5551df42e8a27da5fcb3a4875e02ffcf7eb0a
Prajwal Urs <prajwal@enhanzed.com>
2021-10-19 17:34:44 +0530

    error

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
 }
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 httpServer.listen(port, (error) => {
